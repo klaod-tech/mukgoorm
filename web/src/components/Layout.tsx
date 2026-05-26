867590b9f55ea6fe8878264d55ce06ae5e285f1f
lellon <a67733198@gmail.com>
2026-05-26 11:38:49 +0900

    Updated Changes

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar'
 
 export default function Layout() {
   return (
-    <div style={{ display: 'flex', minHeight: '100vh', color: 'var(--text)' }}>
+    <div style={{ display: 'flex', height: '100vh', overflow: 'hidden', color: 'var(--text)' }}>
       <Sidebar />
       <main style={{ flex: 1, padding: 32, overflowY: 'auto' }}>
         <Outlet />
