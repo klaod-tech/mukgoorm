e53cfd738b485e0ba139ff3673b198084ea154c8
lellon <a67733198@gmail.com>
2026-05-26 10:42:18 +0900

    Updated Changes

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import { T } from '../lib/theme'
 
 export default function Layout() {
   return (
-    <div style={{ display: 'flex', minHeight: '100vh', background: '#0f0f23', color: '#fff' }}>
+    <div style={{ display: 'flex', minHeight: '100vh', background: T.bg, color: T.text }}>
       <Sidebar />
       <main style={{ flex: 1, padding: 32, overflowY: 'auto' }}>
         <Outlet />
